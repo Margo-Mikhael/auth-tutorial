f152abaf70deb9749caf2777a9dfb5d3a1618901
Margo-Mikhael <margomikhaell@gmail.com>
2025-09-05 18:19:22 +0300

    Implement FloatingShape component to use framer-motion for animations

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { motion } from "framer-motion";
 
-export default function FloatingShape() {
-  return <div>FloatingShape</div>;
+export default function FloatingShape({ color, size, top, left, delay }) {
+  return (
+    <motion.div
+      className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
+      style={{ top, left }}
+      animate={{
+        y: ["0%", "100%", "0%"],
+        x: ["0%", "100%", "0%"],
+        rotate: [0, 360],
+      }}
+      transition={{
+        duration: 20,
+        ease: "linear",
+        repeat: Infinity,
+        delay,
+      }}
+      aria-hidden="true"
+    />
+  );
 }
